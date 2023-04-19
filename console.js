db93eea6ddc99172b9fa51ffbf464693f7eb5f45
Fusuf <71168855+cryingcherry@users.noreply.github.com>
2023-04-20 02:10:27 +0300

    Create console.js

---
+++ console.js
@@ -0,0 +1,23 @@
+function countNamedSpans() {
+  const namedSpans = document.querySelectorAll('span[itemprop="name"]');
+  const namedSpansCount = namedSpans.length;
+  console.log(`Sayfadaki entry sayısı: ${namedSpansCount}`);
+}
+
+let isPageEndReached = false;
+
+function checkIfPageEndReached() {
+  if (isPageEndReached) {
+    return;
+  }
+  
+  const loadMoreButton = document.querySelector('.load-more-entries');
+  if (loadMoreButton && !loadMoreButton.classList.contains('hidden')) {
+    loadMoreButton.click();
+  } else {
+    isPageEndReached = true;
+    countNamedSpans();
+  }
+}
+
+setInterval(checkIfPageEndReached, 250);
